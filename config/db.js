dcf2591195f52edf4f93ca83cf3207394359999c
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2022-12-05 20:58:33 -0700

    feat: add db connection and update mongoose version (#2)

---
+++ config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+	try {
+		mongoose.set("strictQuery", true)
+		const db = await mongoose.connect(process.env.MONGO_URI);
+
+		console.log(`MongoDB connected: ${db.connection.host}`);
+	} catch (error) {
+		console.log(error);
+		process.exit(1);
+	}
+};
+
+module.exports = connectDB;
