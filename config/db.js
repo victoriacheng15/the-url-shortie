e1d77a34dd4c573accb066ad802899170c7d428b
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2022-12-05 22:43:54 -0700

    Add url model (#4)
    * feat: change nanoid to v3 for use require
    
    * feat: add shortUrl model
    
    * feat: add routes for shortie and redirect to full url
    
    * feat: display fetch DB info on the page
    
    * style: the code has been formatted by rome

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
 	try {
-		mongoose.set("strictQuery", true)
+		mongoose.set("strictQuery", true);
 		const db = await mongoose.connect(process.env.MONGO_URI);
 
 		console.log(`MongoDB connected: ${db.connection.host}`);
--- index.js
+++ index.js
@@ -1,21 +1,43 @@
 require("dotenv").config();
 const express = require("express");
+const ShortUrl = require("./models/shortUrl");
 
-const connectDB = require('./config/db')
+const connectDB = require("./config/db");
 
-connectDB()
+connectDB();
 
 const app = express();
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-	res.render("index");
+app.get("/", async (req, res) => {
+	const shortUrls = await ShortUrl.find();
+	res.render("index", { shortUrls: shortUrls });
 });
 
-app.post("shortie", (req, res) => {
-	//
+app.post("/shortie", async (req, res) => {
+	await ShortUrl.create({
+		full: req.body.fullUrl,
+	});
+
+	res.redirect("/");
+});
+
+app.get("/:shortUrl", async (req, res) => {
+	const getShort = await ShortUrl.find({ short: req.params.shortUrl });
+	const short = getShort[0];
+
+	if (short === null) {
+		return res.status(404).json({ message: "the url doesnt exist" });
+	}
+
+	short.clicks += 1;
+	await short.save();
+
+	res.redirect(short.full);
 });
 
 app.listen(port, () => {
