3c4c58c615ba581c3135df87a50885ca80c6516d
Victoria Cheng <victoria.cheng15@gmail.com>
2022-12-05 23:05:40 -0700

    feat: use home and shortUrl routes

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 require("dotenv").config();
 const express = require("express");
 const ShortUrl = require("./models/shortUrl");
+const port = process.env.PORT || 5000;
 
 const connectDB = require("./config/db");
+const homeRoute = require('./routes/homeRoute')
+const shortUrlRoute = require('./routes/shortUrlRoute')
 
 connectDB();
 
@@ -11,34 +14,8 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const port = process.env.PORT || 5000;
-
-app.get("/", async (req, res) => {
-	const shortUrls = await ShortUrl.find();
-	res.render("index", { shortUrls: shortUrls });
-});
-
-app.post("/shortie", async (req, res) => {
-	await ShortUrl.create({
-		full: req.body.fullUrl,
-	});
-
-	res.redirect("/");
-});
-
-app.get("/:shortUrl", async (req, res) => {
-	const getShort = await ShortUrl.find({ short: req.params.shortUrl });
-	const short = getShort[0];
-
-	if (short === null) {
-		return res.status(404).json({ message: "the url doesnt exist" });
-	}
-
-	short.clicks += 1;
-	await short.save();
-
-	res.redirect(short.full);
-});
+app.use('/', homeRoute)
+app.use('/', shortUrlRoute)
 
 app.listen(port, () => {
 	console.log(`the server is running on ${port}`);
