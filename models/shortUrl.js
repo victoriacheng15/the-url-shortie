e27fc7be529f209025513b50dc97c642e8522ea9
Victoria Cheng <victoria.cheng15@gmail.com>
2022-12-05 22:41:35 -0700

    feat: add shortUrl model

---
+++ models/shortUrl.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const { nanoid } = require('nanoid');
+
+const shortUrlSchema = new mongoose.Schema({
+	full: {
+		type: String,
+		required: true,
+	},
+	short: {
+		type: String,
+    required: true,
+    default: `https://shortie.${nanoid(6)}`
+	},
+	clicks: {
+		type: Number,
+		required: true,
+		default: 0,
+	},
+	date: {
+		type: String,
+		default: Date.now,
+	},
+});
+
+module.exports = mongoose.model("url", shortUrlSchema);
