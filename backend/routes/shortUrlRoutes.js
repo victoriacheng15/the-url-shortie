f8e5869f964cb196b6dfe6f9762dba43be4eec70
Victoria Cheng <victoria.cheng15@gmail.com>
2022-12-28 14:57:37 -0700

    chore: rename to shortUrlRoutes

--- backend/routes/shortUrlRoutes.js
+++ backend/routes/shortUrlRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { addFull, getShort } = require("../controllers/shortUrlController");
 
-router.post("/fullUrl", addFull);
+router.post("/", addFull);
 router.get("/:shortUrl", getShort);
 
 module.exports = router;
