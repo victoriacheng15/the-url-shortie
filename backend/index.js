7cfbc30d07258dfd433eb38127976d6dbafda2d0
Victoria Cheng <victoria.cheng15@gmail.com>
2022-12-28 12:23:45 -0700

    feat: remove view engine and add api to the route

--- backend/index.js
+++ backend/index.js
@@ -3,18 +3,21 @@ const express = require("express");
 const port = process.env.PORT || 5000;
 
 const connectDB = require("./config/db");
+const { errorMiddleware } = require("./middleware/errorMiddleware");
 const homeRoute = require("./routes/homeRoute");
 const shortUrlRoute = require("./routes/shortUrlRoute");
 
 connectDB();
 
 const app = express();
-// app.set("view engine", "ejs");
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use("/", homeRoute);
-app.use("/", shortUrlRoute);
+app.use("/api", homeRoute);
+app.use("/api", shortUrlRoute);
+
+app.use(errorMiddleware);
 
 app.listen(port, () => {
 	console.log(`the server is running on ${port}`);
