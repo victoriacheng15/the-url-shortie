18a9be27f51a7dade07585e1aa63ce727f3b8d6d
Victoria Cheng <victoria.cheng15@gmail.com>
2022-12-28 12:22:36 -0700

    chore: add desc for each route and logic to check if url exists or not

--- backend/controllers/shortUrlController.js
+++ backend/controllers/shortUrlController.js
@@ -1,33 +1,49 @@
 const ShortUrl = require("../models/shortUrl");
 
-const addFull = async (req, res) => {
-	const { fullUrl } = req.body;
 
-	const newFullUrl = await ShortUrl.find({ full: fullUrl });
+// @desc add a new full url
+// @route POST /api/fullUrl
+// @access private
+const addFull = async (req, res, next) => {
+	const { fullUrl } = req.body;
+	const newUrl = await ShortUrl.findOne({ full: fullUrl });
 
 	try {
-		if (!newFullUrl) {
-			const savedUrl = await newFullUrl.create({ full: fullUrl });
+		if (!newUrl) {
+			const savedUrl = await ShortUrl.create({full: fullUrl});
 			res.status(200).json(savedUrl);
+		} else {
+			res.status(200).json({message: "the url you entered is exist!"})
 		}
-		res.json({ message: "the full url exists" });
 	} catch (error) {
-		console.log(error);
+		next(error);
 	}
 };
 
-const getFull = async (req, res) => {
-	// const getShort = await ShortUrl.find({ short: req.params.shortUrl });
-	// const short = getShort[0];
-	// if (short === null) {
-	// 	return res.status(404).json({ message: "the url doesnt exist" });
-	// }
-	// short.clicks += 1;
-	// await short.save();
-	// res.redirect(short.full);
+
+// @desc get short url
+// @route get /api/:shortUrl
+// @access private
+const getShort = async (req, res) => {
+	try {
+		const {shortUrl} = req.params
+		const getShort = await ShortUrl.find({ short: shortUrl });
+		console.log(getShort)
+		const short = getShort[0];
+		console.log(short);
+		if (short === null) {
+			return res.status(404).json({ message: "the url doesnt exist" });
+		}
+		res.status(200).json(short)
+		// short.clicks += 1;
+		// await short.save();
+		// res.redirect(short.full);
+	} catch (error) {
+		next(error)
+	}
 };
 
 module.exports = {
 	addFull,
-	getFull,
+	getShort,
 };
