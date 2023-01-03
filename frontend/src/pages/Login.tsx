c2f3f0f7cfb771efc7b66d5cbef5847469745a02
Victoria Cheng <victoria.cheng15@gmail.com>
2023-01-03 15:49:42 -0700

    feat: add login ui test for google login

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,5 +1,29 @@
+import { Route } from "react-router-dom";
+import Container from "../components/Container";
+
 function Login() {
-	return <div>Login</div>;
+	const google = () => {
+		window.open("http://localhost:5000/auth/google", "_self");
+	};
+	
+	return (
+		<>
+			<Container>
+				<div className="flex flex-col gap-4 justify-center items-center">
+					<h2 className="text-2xl md:text-4xl text-center">
+						Welcome to Url shortie!
+					</h2>
+					<button
+						type="button"
+						className="text-lg font-medium bg-blue-300 p-2 rounded-lg"
+						onClick={google}
+					>
+						Login with Google
+					</button>
+				</div>
+			</Container>
+		</>
+	);
 }
 
 export default Login;
