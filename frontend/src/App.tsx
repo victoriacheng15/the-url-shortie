bdb75f89caeb9eb5aab705e2dadba4c194b2fbcf
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2023-01-05 18:04:12 -0700

    Edit UI pages (#13)
    * chore: change home to shortie
    
    * chore: rename register to dashboard for user
    
    * chore: change export name to dashboard
    
    * chore: edit route for dashboard
    
    * chore: add main tag for each page
    
    * chore: edit bg color

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,16 @@
 import { Routes, Route } from "react-router-dom";
-import { Home, Register, Login } from "./pages";
+import { Home, Dashboard, Login } from "./pages";
 import { Navbar } from "./components";
 
 function App() {
 	return (
 		<>
 			<Navbar />
-			<main>
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/Register" element={<Register />} />
-					<Route path="/login" element={<Login />} />
-				</Routes>
-			</main>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/Dashboard" element={<Dashboard />} />
+			</Routes>
 		</>
 	);
 }
--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -1,3 +1,3 @@
 export { default as Home } from "./Home";
-export { default as Register } from "./Register";
+export { default as Dashboard } from "./Dashboard";
 export { default as Login } from "./Login";
