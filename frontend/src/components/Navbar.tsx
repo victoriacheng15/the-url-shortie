69fa816e3bd624cc2853a896496e848624342fd7
Victoria Cheng <victoria.cheng15@gmail.com>
2023-01-05 18:01:44 -0700

    chore: change home to shortie

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,12 +4,12 @@ import Container from "./Container";
 function Navbar() {
 	const links = [
 		{
-			label: "home",
+			label: "shortie",
 			href: "/",
 		},
 		{
-			label: "register",
-			href: "/register",
+			label: "dashboard",
+			href: "/dashboard",
 		},
 		{
 			label: "login",
@@ -18,16 +18,24 @@ function Navbar() {
 	];
 
 	return (
-		<nav className="bg-gray-300 py-4">
+		<nav className="bg-gray-900 text-slate-100 py-4">
 			<Container>
 				<ul className="flex gap-4 justify-end">
-					{links.map(({ label, href }) => (
-						<li key={label}>
-							<Link className="capitalize text-lg font-medium" to={href}>
-								{label}
-							</Link>
-						</li>
-					))}
+					{links.map(({ label, href }) =>
+						label === "shortie" ? (
+							<li key={label} className="mr-auto">
+								<Link className="capitalize text-lg font-medium" to={href}>
+									{label}
+								</Link>
+							</li>
+						) : (
+							<li key={label}>
+								<Link className="capitalize text-lg font-medium" to={href}>
+									{label}
+								</Link>
+							</li>
+						),
+					)}
 				</ul>
 			</Container>
 		</nav>
